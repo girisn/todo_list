a381f6c85891cad81599492a446e0c3fa9d20029
girisn <girisn00@gmail.com>
2022-02-27 18:04:51 +0300

    fix error

--- todolist/src/main/resources/todolist-build.sh
+++ todolist/src/main/resources/todolist-build.sh
@@ -2,6 +2,6 @@ rm -rf /tmp/todo_list
 git clone https://github.com/girisn/todo_list.git /tmp/todo_list
 cd /tmp/todo_list
 
-mvn clean package
 cd todolist
+mvn clean package
 docker build -t todolist:1 .
