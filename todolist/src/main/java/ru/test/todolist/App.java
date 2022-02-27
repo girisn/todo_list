719cfc99c48729cacfc7fd47ad6d86bd63e86aea
girisn <girisn00@gmail.com>
2022-02-27 17:22:58 +0300

    config objectMapper

--- todolist/src/main/java/ru/test/todolist/App.java
+++ todolist/src/main/java/ru/test/todolist/App.java
@@ -1,6 +1,8 @@
 package ru.test.todolist;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,7 +17,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class App {
     @Bean
     public ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return objectMapper;
     }
 
     public static void main(String[] args) {
